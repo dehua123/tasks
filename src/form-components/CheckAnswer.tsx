d6d6846cfef3636e2aaa6cf52e58084c4c51178a
dehua123 <mheider@udel.edu>
2022-04-03 17:30:55 -0400

    part of forms

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+            <div>
+                The answer is
+                {answer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}.
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, amountofAttempts] = useState<number>(3);
+    const [request, setRequested] = useState<string>("");
+    const attemptsInt = parseInt(request) || 0;
+
+    const substractOne = (): void => {
+        amountofAttempts(attempts - 1);
+    };
+    const addAttempts = (): void => {
+        amountofAttempts(attempts + attemptsInt);
+    };
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>
+                <Button onClick={substractOne} disabled={attempts === 0}>
+                    use
+                </Button>
+                <Button onClick={addAttempts}>gain</Button>
+            </div>
+            <div>
+                <Form.Group controlId="giveAttempts">
+                    <Form.Label>Requested Attempts:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={request}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setRequested(event.target.value)}
+                    />
+                </Form.Group>
+                <div>{attempts}.</div>{" "}
+            </div>
         </div>
     );
 }
